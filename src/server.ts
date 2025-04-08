4e9cc3bbd61bfdea989e334889780f80d24588b4
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2025-04-08 14:27:02 +0600

    12th attempt

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
 import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
+    AngularNodeAppEngine,
+    createNodeRequestHandler,
+    isMainModule,
+    writeResponseToNodeResponse,
+    CommonEngine,
 } from '@angular/ssr/node';
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+
+import { render } from '@netlify/angular-runtime/common-engine.mjs';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
+
+const commonEngine = new CommonEngine();
 
 /**
  * Example Express Rest API endpoints can be defined here.
@@ -30,23 +35,23 @@ const angularApp = new AngularNodeAppEngine();
  * Serve static files from /browser
  */
 app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
+    express.static(browserDistFolder, {
+        maxAge: '1y',
+        index: false,
+        redirect: false,
+    })
 );
 
 /**
  * Handle all other requests by rendering the Angular application.
  */
 app.use('/**', (req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
+    angularApp
+        .handle(req)
+        .then((response) =>
+            response ? writeResponseToNodeResponse(response, res) : next()
+        )
+        .catch(next);
 });
 
 /**
@@ -54,10 +59,25 @@ app.use('/**', (req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+    const port = process.env['PORT'] || 4000;
+    app.listen(port, () => {
+        console.log(
+            `Node Express server listening on http://localhost:${port}`
+        );
+    });
+}
+export async function netlifyCommonEngineHandler(
+    request: Request,
+    context: any
+): Promise<Response> {
+    // Example API endpoints can be defined here.
+    // Uncomment and define endpoints as necessary.
+    // const pathname = new URL(request.url).pathname;
+    // if (pathname === '/api/hello') {
+    //   return Response.json({ message: 'Hello from the API' });
+    // }
+
+    return await render(commonEngine);
 }
 
 /**
